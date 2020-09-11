2c3a854c783f3f8e9aa4e4a21d0a72fbaedc7a13
PrianiKate <62850134+PrianiKate@users.noreply.github.com>
2020-09-11 19:11:59 +0400

    feat: add main file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let l = [];
+  let r = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        l.push(bracketsConfig[i][0]);
+        r.push(bracketsConfig[i][1]);
+    }
+
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        let posr = r.indexOf(str[i]);
+        let posl = l.indexOf(str[i]);
+
+        if (posr !== -1) {
+            let top = stack[stack.length - 1];
+
+            if (posr === posl && (top !== str[i] || stack.length === 0)) {
+                stack.push(str[i]);
+            } else if (l.indexOf(top) === posr) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        } else {
+            stack.push(str[i]);
+        }
+    }
+
+    return stack.length === 0;
 }
